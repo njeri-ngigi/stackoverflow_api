4c83a8162680daa03aff44f40793410d11cda19b
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-30 17:09:46 +0300

    [CHORE] Add unittest for posting and accepting answers endpoint

--- tests/test_question_answers.py
+++ tests/test_question_answers.py
@@ -23,9 +23,11 @@ class TestQuestionAnswers(unittest.TestCase):
                             data=json.dumps({"username": "suite", "password": "Test123"}))
         u_data2 = ast.literal_eval(res2.data)
         self.a_token2 = u_data2["token"]
-        #user1 posts a question
+        #user1 posts 2 questions
         self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json",
                             data=json.dumps({"title": "Git branching", "content": "How to create and checkout a branch in git"}))
+        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json",
+                            data=json.dumps({"title": "Baking", "content": "How much baking soda to use?"}))
 
     def test_post_answer(self):
         '''test handling posting an answer'''
@@ -42,8 +44,47 @@ class TestQuestionAnswers(unittest.TestCase):
         self.assertEqual(result2.status_code, 404)
         self.assertEqual("Question doesn't exist", my_data2["message"])
 
-    def test_accept_answer(self):
-        '''test accepting answer'''
-
-    def test_update_answer(self):
-        '''test updating answer'''
+    def test_accept_update_answer(self):
+        '''test accepting and updating answer'''  
+        #user2 posts answer
+        self.client().post('/api/v1/questions/1/answers', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json",
+                            data=json.dumps({"content": "1 tsp per 3 cups of flour"})) 
+        #test successful accept answer      
+        result = self.client().put('/api/v1/questions/1/answers/0', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json",
+                            data=json.dumps({})) 
+        my_data = ast.literal_eval(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual("Answer #0 accepted!", my_data["message"])
+        #test unauthorized accepting of answer
+        result2 = self.client().put('/api/v1/questions/1/answers/0', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json",
+                            data=json.dumps({})) 
+        my_data2 = ast.literal_eval(result2.data)
+        self.assertEqual(result2.status_code, 401)
+        self.assertEqual("Unauthorized to accept answer", my_data2["message"])
+        #test successful update answer
+        result3 = self.client().put('/api/v1/questions/1/answers/0', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json",
+                            data=json.dumps({"content": "2 tsp per 3 cups of flour"})) 
+        my_data3 = ast.literal_eval(result3.data)
+        self.assertEqual(result3.status_code, 200)
+        self.assertEqual("Answer updated!", my_data3["message"])
+        
+        #test unauthorized updating answer
+        result4 = self.client().put('/api/v1/questions/1/answers/0', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json",
+                            data=json.dumps({"content": "2 tsp per 3 cups of flour"})) 
+        my_data4 = ast.literal_eval(result4.data)
+        self.assertEqual(result4.status_code, 401)
+        self.assertEqual("Unauthorized to edit answer", my_data4["message"])
+        
+        #test question doesn't exist
+        result5 = self.client().put('/api/v1/questions/10/answers/0', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json",
+                            data=json.dumps({"content": "2 tsp per 3 cups of flour"})) 
+        my_data5 = ast.literal_eval(result5.data)
+        self.assertEqual(result5.status_code, 404)
+        self.assertEqual("Question doesn't exist", my_data5["message"])
+        #test answer doesn't exist
+        result6 = self.client().put('/api/v1/questions/1/answers/10', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json",
+                            data=json.dumps({"content": "2 tsp per 3 cups of flour"})) 
+        my_data6 = ast.literal_eval(result6.data)
+        self.assertEqual(result6.status_code, 404)
+        self.assertEqual("This answer doesn't exist", my_data6["message"])
+        
