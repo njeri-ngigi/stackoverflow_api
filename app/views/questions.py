b63966816e03034202533c68bd7fca1d06da530e
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-30 17:11:32 +0300

    [FEATURE] Add endpoints for posting and accepting answers

--- app/views/questions.py
+++ app/views/questions.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from flask import request, jsonify
-from flask_jwt_extended import (jwt_required, get_raw_jwt, get_jwt_claims, get_jwt_identity)
+from flask_jwt_extended import (jwt_required, get_raw_jwt, get_jwt_identity)
 import ast
 
 from app import User, Question
@@ -12,7 +12,7 @@ class Questions(Resource):
             return result, 200
         all_questions = []
         for i in result:
-            question = dict(title=i.title, content=i.content, username=i.username)
+            question = dict(title=i.title, content=i.content, username=i.username, answers=i.answers, accepted_answer=i.answer_accepted)
             all_questions.append(question)
         return all_questions, 200
 
@@ -41,7 +41,7 @@ class QuestionsQuestionId(Resource):
         if "message" in u_question:
             return dict(message=u_question["message"]), u_question["error"]
         q = u_question["q"]
-        result = dict(title=q.title, content=q.content, username=q.username)
+        result = dict(title=q.title, content=q.content, username=q.username, answers=q.answers, accepted_answer=q.answer_accepted)
         
         return result, 200
 
@@ -53,3 +53,42 @@ class QuestionsQuestionId(Resource):
         if "error" in result:
             return dict(message=result["message"]), result["error"]
         return result, 200
+
+class QuestionsAnswers(Resource):
+    @jwt_required
+    def post(self, question_id):
+        data = request.get_json()
+        if not data:
+            return dict(message="Field(s) cannot be empty")
+        content = data.get("content")
+        if not content:
+            return dict(message="Please enter answer content")
+        username = get_jwt_identity()
+        q_id = ast.literal_eval(question_id)
+        result = User.postAnswer(q_id, username, content)
+        if "error" in result:
+            return dict(message=result["message"]), result["error"]
+        return result, 201
+
+class QuestionsAnswersId(Resource):
+    @jwt_required
+    def put(self, question_id, answer_id):
+        data = request.get_json()
+        q_id = ast.literal_eval(question_id)
+        a_id = ast.literal_eval(answer_id)
+        username = get_jwt_identity()
+            
+        if data: 
+            content = data.get("content")
+            if not content:
+                return dict(message="Please enter answer content")
+            result = User.updateAnswer(q_id, a_id, username, content)
+
+            if "error" in result:
+                return dict(message=result["message"]), result["error"]
+            return result, 200
+
+        result2 = User.acceptAnswer(q_id, a_id, username)
+        if "error" in result2:
+            return dict(message=result2["message"]), result2["error"]
+        return result2, 200
