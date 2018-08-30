53b53f96c00bfd00c7fedf605689a2f0181b0cd9
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-30 13:41:25 +0300

    [CHORE] Add unittest for deleting a question

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -19,16 +19,17 @@ class TestQuestions(unittest.TestCase):
         res = self.client().post('/api/v1/auth/login', content_type="application/json", 
                             data=json.dumps({"username": "njery", "password": "Test123"}))
         u_data = ast.literal_eval(res.data)
-        a_token = u_data["token"]
+        self.a_token = u_data["token"]
 
         res2 = self.client().post('/api/v1/auth/login', content_type="application/json", 
                             data=json.dumps({"username": "suite", "password": "Test123"}))
         u_data2 = ast.literal_eval(res2.data)
-        a_token2 = u_data2["token"]
+        self.a_token2 = u_data2["token"]
         #post 3 questions
-        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + a_token), content_type="application/json", data=json.dumps({"title": "Git branching", "content": "How to create and checkout a branch in git"}))
-        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + a_token2), content_type="application/json", data=json.dumps({"title": "How to make a github page?", "content": "How do you host web templates on github using github pages"}))
-
+        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json", data=json.dumps({"title": "Git branching", "content": "How to create and checkout a branch in git"}))
+        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json", data=json.dumps({"title": "How to make a github page?", "content": "How do you host web templates on github using github pages"}))
+        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json", data=json.dumps({"title": "Baby Shower", "content": "How to throw a greate baby shower"}))
+        
 
     def test_questions(self):
         '''test handling posting questions'''
@@ -56,4 +57,30 @@ class TestQuestions(unittest.TestCase):
 
     def test_delete_questions(self):
         '''test deleting questions'''
+        #test successful delete
+        result = self.client().get('/api/v1/questions')
+        my_data = ast.literal_eval(result.data)
+        current_length = len(my_data)
+
+        result2 = self.client().delete('/api/v1/questions/2', headers=dict(Authorization="Bearer " + self.a_token2))
+        my_data2 = ast.literal_eval(result2.data)
+        self.assertEqual("Question #2 Deleted Successfully", my_data2["message"])
+        self.assertEqual(result2.status_code, 200)
+
+        result3 = self.client().get('/api/v1/questions')
+        my_data3 = ast.literal_eval(result3.data)
+        self.assertEqual(len(my_data3), current_length-1)
+
+        #test unauthorized delete
+        result4 = self.client().delete('/api/v1/questions/0', headers=dict(Authorization="Bearer " + self.a_token2))
+        my_data4 = ast.literal_eval(result4.data)
+        self.assertEqual("Unauthorized to delete this question", my_data4["message"])
+        self.assertEqual(result4.status_code, 401)
+
+        #test missing question
+        result5 = self.client().delete('/api/v1/questions/10', headers=dict(Authorization="Bearer " + self.a_token2))
+        my_data5 = ast.literal_eval(result5.data)
+        self.assertEqual("Question doesn't exist", my_data5["message"])
+        self.assertEqual(result5.status_code, 404)
+
 
