ebdd63e43c9de6da93fb3f0af35650571f52b1da
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-31 08:39:22 +0300

    [CHORE] Adjust files to accomodate pylint recommendations

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 '''./run.py'''
-from app import createApp
 from flask import jsonify
+from app import createApp
 
 config_name = "development"
 app = createApp(config_name)
@@ -9,21 +9,22 @@ app = createApp(config_name)
 @app.errorhandler(400)
 def bad_request(error):
     '''Bad request'''
-    return jsonify(dict(error = 'Bad request')), 400
+    return jsonify(dict(error='Bad request')), 400
 @app.errorhandler(404)
 def page_not_found(error):
     '''Page not found'''
-    return jsonify(dict(error = 'Page not found')), 404
+    return jsonify(dict(error='Page not found')), 404
 
 @app.errorhandler(405)
 def unauthorized_method(error):
     '''Unauthorized method'''
-    return jsonify(dict(error = 'Method not allowed')), 405
+    return jsonify(dict(error='Method not allowed')), 405
 
 @app.errorhandler(500)
 def server_error(error):
     '''Internal server error'''
-    return jsonify(dict(error = 'Internal server error')), 500
+    return jsonify(dict(error='Internal server error')), 500
 
 if __name__ == "__main__":
     app.run()
+    
