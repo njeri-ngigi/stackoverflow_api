4915777d534b50bca22e7b8e86927f43cc7db84c
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-31 08:38:58 +0300

    [CHORE] Adjust files to accomodate pylint recommendations

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -10,33 +10,47 @@ class TestQuestions(unittest.TestCase):
         self.app = createApp(config_name="testing")
         self.client = self.app.test_client
 
-        #register 2 users 
-        self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test123"}))
-        self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Suite", "username": "suite", 
-                            "email": "suite@to.com", "password": "Test123", "confirm_password": "Test123"}))        
+        #register 2 users
+        self.client().post('/api/v1/auth/signup', content_type="application/json",
+                           data=json.dumps({"name": "Njeri", "username": "njery",
+                                            "email": "njeri@to.com", "password": "Test123",
+                                            "confirm_password": "Test123"}))
+        self.client().post('/api/v1/auth/signup', content_type="application/json",
+                           data=json.dumps({"name": "Suite", "username": "suite",
+                                            "email": "suite@to.com", "password": "Test123",
+                                            "confirm_password": "Test123"}))
         #login in both users
-        res = self.client().post('/api/v1/auth/login', content_type="application/json", 
-                            data=json.dumps({"username": "njery", "password": "Test123"}))
+        res = self.client().post('/api/v1/auth/login', content_type="application/json",
+                                 data=json.dumps({"username": "njery", "password": "Test123"}))
         u_data = ast.literal_eval(res.data)
         self.a_token = u_data["token"]
 
-        res2 = self.client().post('/api/v1/auth/login', content_type="application/json", 
-                            data=json.dumps({"username": "suite", "password": "Test123"}))
+        res2 = self.client().post('/api/v1/auth/login', content_type="application/json",
+                                  data=json.dumps({"username": "suite", "password": "Test123"}))
         u_data2 = ast.literal_eval(res2.data)
         self.a_token2 = u_data2["token"]
         #post 3 questions
-        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token), content_type="application/json", data=json.dumps({"title": "Git branching", "content": "How to create and checkout a branch in git"}))
-        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json", data=json.dumps({"title": "How to make a github page?", "content": "How do you host web templates on github using github pages"}))
-        self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token2), content_type="application/json", data=json.dumps({"title": "Baby Shower", "content": "How to throw a greate baby shower"}))
-        
+        self.client().post('/api/v1/questions',
+                           headers=dict(Authorization="Bearer " + self.a_token),
+                           content_type="application/json",
+                           data=json.dumps({"title": "Git branching", "content": "How to create and checkout a branch in git"}))
+        self.client().post('/api/v1/questions',
+                           headers=dict(Authorization="Bearer " + self.a_token2),
+                           content_type="application/json",
+                           data=json.dumps({"title": "How to make a github page?",
+                                            "content": "How do you host web templates on github using github pages"}))
+        self.client().post('/api/v1/questions',
+                           headers=dict(Authorization="Bearer " + self.a_token2),
+                           content_type="application/json",
+                           data=json.dumps({"title": "Baby Shower",
+                                            "content": "How to throw a greate baby shower"}))
     def test_questions(self):
         '''test handling posting questions'''
-        result = self.client().post('/api/v1/questions', headers=dict(Authorization="Bearer " + self.a_token), 
-                                    content_type="application/json", 
-                                    data=json.dumps({"title": "Baking a sponge cake", "content": "When baking a sponge cake how many eggs do you put in?"}))
+        result = self.client().post('/api/v1/questions',
+                                    headers=dict(Authorization="Bearer " + self.a_token),
+                                    content_type="application/json",
+                                    data=json.dumps({"title": "Baking a sponge cake",
+                                                     "content": "When baking a sponge cake how many eggs do you put in?"}))
         my_data = ast.literal_eval(result.data)
         self.assertEqual(result.status_code, 201)
         self.assertEqual("Baking a sponge cake, Posted!", my_data["message"])
@@ -68,7 +82,8 @@ class TestQuestions(unittest.TestCase):
         my_data = ast.literal_eval(result.data)
         current_length = len(my_data)
 
-        result2 = self.client().delete('/api/v1/questions/2', headers=dict(Authorization="Bearer " + self.a_token2))
+        result2 = self.client().delete('/api/v1/questions/2',
+                                       headers=dict(Authorization="Bearer " + self.a_token2))
         my_data2 = ast.literal_eval(result2.data)
         self.assertEqual("Question #2 Deleted Successfully", my_data2["message"])
         self.assertEqual(result2.status_code, 200)
@@ -78,13 +93,15 @@ class TestQuestions(unittest.TestCase):
         self.assertEqual(len(my_data3), current_length-1)
 
         #test unauthorized delete
-        result4 = self.client().delete('/api/v1/questions/0', headers=dict(Authorization="Bearer " + self.a_token2))
+        result4 = self.client().delete('/api/v1/questions/0',
+                                       headers=dict(Authorization="Bearer " + self.a_token2))
         my_data4 = ast.literal_eval(result4.data)
         self.assertEqual("Unauthorized to delete this question", my_data4["message"])
         self.assertEqual(result4.status_code, 401)
 
         #test missing question
-        result5 = self.client().delete('/api/v1/questions/10', headers=dict(Authorization="Bearer " + self.a_token2))
+        result5 = self.client().delete('/api/v1/questions/10',
+                                       headers=dict(Authorization="Bearer " + self.a_token2))
         my_data5 = ast.literal_eval(result5.data)
         self.assertEqual("Question doesn't exist", my_data5["message"])
         self.assertEqual(result5.status_code, 404)
--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -13,127 +13,133 @@ class RegisterUserTestCase(unittest.TestCase):
     def test_register_user(self):
         '''Test handling user registration'''
         #test successful registration
-        result = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test123"}))
+        result = self.client().post('/api/v1/auth/signup',
+                                    content_type="application/json",
+                                    data=json.dumps({"name": "Njeri", "username": "njery",
+                                                     "email": "njeri@to.com", "password": "Test123",
+                                                     "confirm_password": "Test123"}))
         my_data = ast.literal_eval(result.data)
-
         self.assertEqual(result.status_code, 201)
         self.assertEqual("Welcome njery!", my_data["message"])
-
         #test registration using the same username
-        result2 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test123"}))
+        result2 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "njeri@to.com", "password": "Test123",
+                                                      "confirm_password": "Test123"}))
         my_data2 = ast.literal_eval(result2.data)
-
         self.assertEqual(result2.status_code, 409)
         self.assertEqual("Username already exists. Try a different one.", my_data2["message"])
-
-        #test missing fields 
-        result3 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({}))
+        #test missing fields
+        result3 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({}))
         my_data3 = ast.literal_eval(result3.data)
-
         self.assertEqual(result3.status_code, 400)
         self.assertEqual("Fields cannot be empty", my_data3["message"])
-
         #test missing data fields
-        result4 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery"}))
+        result4 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery"}))
         my_data4 = ast.literal_eval(result4.data)
-
         self.assertEqual(result4.status_code, 400)
         self.assertEqual("name, username, email, password or confirm_password fields missing", my_data4["message"])
 
     def test_validate_data(self):
         '''Test validating user input'''
         #test invalid name with special characters
-        result = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri@#", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test123"}))
+        result = self.client().post('/api/v1/auth/signup',
+                                    content_type="application/json",
+                                    data=json.dumps({"name": "Njeri@#", "username": "njery",
+                                                     "email": "njeri@to.com", "password": "Test123",
+                                                     "confirm_password": "Test123"}))
         my_data = ast.literal_eval(result.data)
-
         self.assertEqual(result.status_code, 400)
         self.assertEqual("Name cannot contain special characters and numbers", my_data["message"])
         #test invalid email
-        result2 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "abc", "password": "Test123", "confirm_password": "Test123"}))
+        result2 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "abc", "password": "Test123",
+                                                      "confirm_password": "Test123"}))
         my_data2 = ast.literal_eval(result2.data)
-
         self.assertEqual(result2.status_code, 400)
         self.assertEqual("Enter a valid email address", my_data2["message"])
         #test short password
-        result3 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "123", "confirm_password": "123"}))
+        result3 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "njeri@to.com", "password": "123",
+                                                      "confirm_password": "123"}))
         my_data3 = ast.literal_eval(result3.data)
-
         self.assertEqual(result3.status_code, 400)
         self.assertEqual("password is too short", my_data3["message"])
         #test weak password
-        result4 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "123456", "confirm_password": "123456"}))
+        result4 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "njeri@to.com", "password": "123456",
+                                                      "confirm_password": "123456"}))
         my_data4 = ast.literal_eval(result4.data)
-
         self.assertEqual(result4.status_code, 400)
         self.assertEqual("password must contain a mix of upper and lowercase letters", my_data4["message"])
-
-        result5 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Testing", "confirm_password": "Testing"}))
+        #test weak password
+        result5 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "njeri@to.com", "password": "Testing",
+                                                      "confirm_password": "Testing"}))
         my_data5 = ast.literal_eval(result5.data)
-
         self.assertEqual(result5.status_code, 400)
         self.assertEqual("password must contain atleast one numeric or special character", my_data5["message"])
         #test unmatching passwords
-        result6 = self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Njeri", "username": "njery", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test13"}))
+        result6 = self.client().post('/api/v1/auth/signup',
+                                     content_type="application/json",
+                                     data=json.dumps({"name": "Njeri", "username": "njery",
+                                                      "email": "njeri@to.com", "password": "Test123",
+                                                      "confirm_password": "Test13"}))
         my_data6 = ast.literal_eval(result6.data)
-
         self.assertEqual(result6.status_code, 400)
         self.assertEqual("Passwords don't match", my_data6["message"])
 
     def test_login_logout(self):
         '''Test user login and logout'''
-        self.client().post('/api/v1/auth/signup', content_type="application/json", 
-                            data=json.dumps({"name": "Shalon", "username": "shanje", 
-                            "email": "njeri@to.com", "password": "Test123", "confirm_password": "Test123"}))
+        self.client().post('/api/v1/auth/signup',
+                           content_type="application/json",
+                           data=json.dumps({"name": "Shalon", "username": "shanje",
+                                            "email": "njeri@to.com", "password": "Test123",
+                                            "confirm_password": "Test123"}))
         #test successful login
-        result = self.client().post('/api/v1/auth/login', content_type="application/json",
-                            data=json.dumps({"username": "shanje", "password":"Test123"}))
+        result = self.client().post('/api/v1/auth/login',
+                                    content_type="application/json",
+                                    data=json.dumps({"username": "shanje", "password":"Test123"}))
         my_data = ast.literal_eval(result.data)
         a_token = my_data["token"]
-
         self.assertEqual(result.status_code, 200)
         self.assertEqual("Welcome back, shanje!", my_data["message"])
         self.assertIn("token", my_data)
         #test wrong username
-        result2 = self.client().post('/api/v1/auth/login', content_type="application/json",
-                            data=json.dumps({"username": "none", "password":"Test123"}))
+        result2 = self.client().post('/api/v1/auth/login',
+                                     content_type="application/json",
+                                     data=json.dumps({"username": "none", "password":"Test123"}))
         my_data2 = ast.literal_eval(result2.data)
-
         self.assertEqual(result2.status_code, 401)
         self.assertEqual("Username doesn't exixt. Try Signing up.", my_data2["message"])
         #test wrong password
-        result3 = self.client().post('/api/v1/auth/login', content_type="application/json",
-                            data=json.dumps({"username": "shanje", "password":"Testing"}))
+        result3 = self.client().post('/api/v1/auth/login',
+                                     content_type="application/json",
+                                     data=json.dumps({"username": "shanje", "password":"Testing"}))
         my_data3 = ast.literal_eval(result3.data)
-
         self.assertEqual(result3.status_code, 401)
         self.assertEqual("Incorrect password", my_data3["message"])
         #test logout
-        result4 = self.client().post('/api/v1/auth/logout', headers=dict(Authorization="Bearer " + a_token))
-        result5 = self.client().post('/api/v1/auth/logout', headers=dict(Authorization="Bearer " + a_token))
-        
+        result4 = self.client().post('/api/v1/auth/logout',
+                                     headers=dict(Authorization="Bearer " + a_token))
+        result5 = self.client().post('/api/v1/auth/logout',
+                                     headers=dict(Authorization="Bearer " + a_token))
         my_data4 = ast.literal_eval(result4.data)
         my_data5 = ast.literal_eval(result5.data)
-
         self.assertEqual(result4.status_code, 200)
         self.assertEqual(result5.status_code, 401)
-       
         self.assertEqual("Leaving so soon?", my_data4["message"])
         self.assertEqual("Token has been revoked", my_data5["msg"])
