f0316e16fff113bb027f165d09010f81a38486ed
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-09-10 09:55:22 +0300

    [CHORE] Use dotenv module to load .env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
 '''app/__init__.py'''
+import os
+from dotenv import load_dotenv
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
+# Create .env file path.
+dotenv_path = os.path.join(APP_ROOT, '.env')
+# Load file from the path.
+load_dotenv(dotenv_path)
