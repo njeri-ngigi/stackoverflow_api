4751f418e81d974aaa9db6b32c308cb045ba1d58
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-09-11 11:28:16 +0300

    [CHORE] Add import for UserQuestions

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,4 +1,4 @@
 '''view/__init__.py'''
 from app.views.user_registration import Signup, Login, Logout
 from app.views.questions import (Questions, QuestionsQuestionId, QuestionsAnswers, QuestionsAnswersId, 
-                                 QuestionsAnswersUpvote, QuestionsAnswersDownvote)
+                                 QuestionsAnswersUpvote, QuestionsAnswersDownvote, UserQuestions)
