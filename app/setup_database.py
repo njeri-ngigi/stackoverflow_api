a46ad987b02bb46e79cd08843769f3ecf1f483b9
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-09-11 11:23:50 +0300

    [FEATURE] Create comments table

--- app/setup_database.py
+++ app/setup_database.py
@@ -13,11 +13,12 @@ class SetupDB(object):
         cursor = db_connection.cursor()
 
         cursor.execute('''CREATE TABLE IF NOT EXISTS questions(
-                question_id   SERIAL PRIMARY KEY,
-                q_title       VARCHAR(50)  NOT NULL,
-                q_content     VARCHAR(200)  NOT NULL,
-                q_username    VARCHAR(20) NOT NULL,
-                q_accepted_answer INTEGER DEFAULT 0
+                question_id          SERIAL PRIMARY KEY,
+                q_title              VARCHAR(50)  NOT NULL,
+                q_content            VARCHAR(200)  NOT NULL,
+                q_username           VARCHAR(20) NOT NULL,
+                q_accepted_answer    INTEGER DEFAULT 0,
+                q_answers            INTEGER DEFAULT 0
                 );''')
 
         cursor.execute('''CREATE TABLE IF NOT EXISTS users(
@@ -51,6 +52,15 @@ class SetupDB(object):
                 vote          INTEGER DEFAULT 0,
                 PRIMARY KEY   (vote_id, a_id)
                 );''')
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS comments(
+                comment_id    SERIAL UNIQUE NOT NULL,
+                a_id          INTEGER REFERENCES answers(answer_id) ON DELETE CASCADE,
+                c_username    VARCHAR(20) NOT NULL,
+                c_content     VARCHAR(200) NOT NULL,
+                PRIMARY KEY   (comment_id, a_id)
+                );''')
+        
 
         db_connection.commit()
         cursor.close()
