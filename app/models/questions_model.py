af0aca416f9583a1482bfb4da74e4d20f519c58e
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-09-16 10:14:11 +0300

    [CHORE] Refactor questions_model.py

--- app/models/questions_model.py
+++ app/models/questions_model.py
@@ -11,8 +11,9 @@ class QuestionsModel(BaseModel):
         self.cursor.execute("SELECT * FROM questions WHERE q_title = (%s);", (title, ))
         result = self.cursor.fetchone()
         if result:
-            return dict(message="Question has already been asked. Visit question #" +
-                        str(result[0]), question_id=result[0], error=409)
+            my_resp = dict(message="Question has already been asked. Visit question #" +
+                        str(result[0]), question_id=result[0])
+            return dict(response=my_resp, status_code=409)
         sql = """INSERT INTO questions(q_title, q_content, q_username)
                  VALUES(%s, %s, %s) RETURNING question_id;"""
         self.cursor.execute(sql, (title, content, username))
@@ -20,8 +21,8 @@ class QuestionsModel(BaseModel):
         self.conn.commit()
         self.cursor.close()
         if not q_id:
-            return dict(message="Failed to add question. Try again.", error=404)
-        return dict(message=title + ", Posted!")
+            return dict(response=dict(message="Failed to add question. Try again."), status_code=500)
+        return dict(response=dict(message=title + ", Posted!"), status_code=201)
 
     def get_all_questions(self, limit=None, most_answers=None):
         '''get all questions'''
@@ -43,8 +44,8 @@ class QuestionsModel(BaseModel):
         result = self.cursor.fetchone()
         self.conn.close()
         if not result:
-            return dict(message="Question doesn't exist", error=404)
-        return result
+            return dict(response=dict(message="Question doesn't exist"), status_code=404)
+        return dict(response=dict(title=result[1], content=result[2], username=result[3]), status_code=200)
 
     def delete_question(self, question_id, username):
         '''Delete question'''
@@ -52,10 +53,10 @@ class QuestionsModel(BaseModel):
         result = self.cursor.fetchone()
         if not result:
             self.conn.close()
-            return dict(message="Question doesn't exist", error=404)
+            return dict(response=dict(message="Question doesn't exist"), status_code=404)
         if username != result[3]:
             self.conn.close()
-            return dict(message="Unauthorized to delete this question", error=401)
+            return dict(response=dict(message="Unauthorized to delete this question"), status_code=401)
         self.cursor.execute("DELETE FROM questions WHERE question_id = (%s);", (question_id,))
         self.conn.commit()
         # check if question is deleted
@@ -63,8 +64,8 @@ class QuestionsModel(BaseModel):
         result2 = self.cursor.fetchone()
         self.conn.close()
         if result2:
-            return dict(message="Failed to delete question. Try again.")
-        return dict(message="Question " + "#" + str(question_id) + " Deleted Successfully")
+            return dict(response=dict(message="Failed to delete question. Try again."), status_code=500)
+        return dict(response=dict(message="Question " + "#" + str(question_id) + " Deleted Successfully"), status_code=200)
 
     def get_all_user_questions(self, username, limit=None):
         '''get all questions a user has ever asked'''
@@ -83,12 +84,12 @@ class QuestionsModel(BaseModel):
         result = self.cursor.fetchone()
         if result:
             self.conn.close()
-            return dict(question_id=result[0], title=result[1])
+            return dict(response=dict(question_id=result[0], title=result[1]), status_code=200)
         self.cursor.execute("SELECT q_title FROM questions")
         result = self.cursor.fetchmany(limit)
         flattened_list = list(chain.from_iterable(result))
         search_result = get_close_matches(title, flattened_list, n=15)
         self.conn.close()
         if not search_result:
-            return dict(message="No matches. Be the first to ask the question?", error=404)
-        return search_result
+            return dict(response=dict(message="No matches. Be the first to ask the question?"), status_code=404)
+        return dict(response=search_result, status_code=200)
